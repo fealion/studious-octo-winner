2b7da61bbb63fb4e604651140ce438a034b3ef90
fealion <spideypoole@gmail.com>
2026-06-19 00:54:05 -0400

    Add files via upload

---
+++ js/layout.js
@@ -0,0 +1,101 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Page has finished loading. Now, do things.
+  loadLayoutByPetraPixel();
+
+  // Add any custom JavaScript code here...
+});
+
+function loadLayoutByPetraPixel() {
+  const mainEl = document.querySelector("main");
+  if (!mainEl) return;
+  mainEl.insertAdjacentHTML("afterbegin", headerHTML());
+  mainEl.insertAdjacentHTML("beforeend", footerHTML());
+  giveActiveClassToCurrentPage();
+}
+
+const nesting = getNesting();
+
+function headerHTML() {
+  // ${nesting} outputs "./" or "../" depending on current page depth.
+  // You can use it to refer to images etc.
+  // Example: <img src="${nesting}img/logo.png"> might output <img src="../img/logo.png">
+
+  return `
+  
+      <!-- =============================================== -->
+      <!-- HEADER -->
+      <!-- =============================================== -->
+
+      <header>
+<div class="header-image">
+          <img src="https://raw.githubusercontent.com/fealion/studious-octo-winner/refs/heads/main/images/larp/title%402x.png" alt="" />
+        </div>
+        <div class="header-content">
+
+        </div>
+      </header>
+
+	  
+	
+      `;
+}
+
+function footerHTML() {
+  // ${nesting} outputs "./" or "../" depending on current page depth.
+  // You can use it to refer to images etc.
+  // Example: <img src="${nesting}img/logo.png"> might output <img src="../img/logo.png">
+
+  return `
+
+
+      <!-- =============================================== -->
+      <!-- FOOTER -->
+      <!-- =============================================== -->
+
+      <footer>
+            <div> ⊗ 2026 larpy Huang (<a href="https://www.youtube.com/@LarpyBH">Larpy's channal</a>)and Miclarp Huang</div>
+			<div></div>
+      </footer>`;
+}
+
+/* Do not edit anything below this line unless you know what you're doing. */
+
+function giveActiveClassToCurrentPage() {
+  const els = document.querySelectorAll("nav a");
+  [...els].forEach((el) => {
+    const href = el.getAttribute("href").replace(".html", "").replace("#", "");
+    const pathname = window.location.pathname.replace("/public/", "");
+    const currentHref = window.location.href.replace(".html", "") + "END";
+
+	/* Homepage */
+    if (href == "/" || href == "/index.html") {
+      if (pathname == "/") {
+        el.classList.add("active");
+      }
+    } else {
+      /* Other pages */
+      if (currentHref.includes(href + "END")) {
+        el.classList.add("active");
+
+        /* Subnavigation: */
+		
+        if (el.closest("details")) {
+          el.closest("details").setAttribute("open", "open");
+          el.closest("details").classList.add("active");
+        }
+
+        if (el.closest("ul")) {
+          if (el.closest("ul").closest("ul")) {
+          	el.closest("ul").closest("ul").classList.add("active");
+          }
+        }
+      }
+    }
+  });
+}
+
+function getNesting() {
+  const numberOfSlashes = window.location.pathname.split("/").length - 1;
+  if (numberOfSlashes == 1) return "./";
+  return "../".repeat(numberOfSlashes - 1);
+}
